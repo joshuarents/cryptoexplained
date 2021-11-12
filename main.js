509cc3d1ac5c55937d8027cbf2a18d0e471a4007
joshuarents <66503887+joshuarents@users.noreply.github.com>
2021-11-12 16:58:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Scripting
-gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger); // registering the gsap plugins 
 
 window.scroll(0, document.documentElement.scrollHeight) // Scroll to bottom as soon as page loads
 
-function rotate() {
+function rotate() { // function to rotate rocket clockwise and counterclockwise 29 times to simulate taking off
   gsap.to("#rocket", {
     rotation: 3,
     duration: 0.1,
@@ -13,7 +13,7 @@ function rotate() {
 
 function launch() {              // funtion called when clicking begin button to scroll smoothly down to budget section
   rotate()
-  //setTimeout(() => {  gsap.to("#rocket", {duration: 6, rotation: 0,  y: -3600}); }, 3000);
+
   setTimeout(() => {  gsap.to("#rocket", {duration: 0.6, ease: "none", rotation: 0,  y: -400}); }, 3000);
   setTimeout(() => {  gsap.to(window, {duration: 5, scrollTo:{y:"#whatIsCrypto"}});  }, 3600);
   setTimeout(() => {  document.getElementById("rocket").style.visibility = "hidden" , 
@@ -21,7 +21,7 @@ function launch() {              // funtion called when clicking begin button to
 
 }
 
-gsap.utils.toArray(".cloudsLeft").forEach((cloud) => {
+gsap.utils.toArray(".cloudsLeft").forEach((cloud) => { // translating each cloud in cloudsLeft class left along x axis
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: cloud,
@@ -35,7 +35,7 @@ gsap.utils.toArray(".cloudsLeft").forEach((cloud) => {
   })
 })
 
-gsap.utils.toArray(".cloudsRight").forEach((cloud) => {
+gsap.utils.toArray(".cloudsRight").forEach((cloud) => { // translating each cloud in cloudsRight class right along x axis
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: cloud,
@@ -49,7 +49,9 @@ gsap.utils.toArray(".cloudsRight").forEach((cloud) => {
   })
 })
 
-gsap.to(".plane", {
+// I know this is not efficient but I could not figure out a way to differentiate between the clouds to make them go opposite directions
+
+gsap.to(".plane", { // script to move plane when it is in the viewport
   scrollTrigger: {
     trigger: ".plane",
     start: "top 0%",
@@ -59,7 +61,7 @@ gsap.to(".plane", {
   y: 200,
 })
 
-gsap.to(".satellite", {
+gsap.to(".satellite", { // script to move satellite when it is in the viewport
   scrollTrigger: {
     trigger: ".satellite",
     start: "top 0%",
